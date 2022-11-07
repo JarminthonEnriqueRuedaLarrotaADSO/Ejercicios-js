f347694b284e4ea8c49f32f88b5135a8f557eeef
JaminthonEnriqueRuedaLarrotaADSO <pocopoc4158j@gmail.com>
2022-11-07 10:52:01 -0500

    Punto 3

--- main.js
+++ main.js
@@ -1,14 +1,27 @@
 addEventListener("DOMContentLoaded", (e) => {
-let pagar = document.querySelector('#pagar')
+
 let formulario = document.querySelector("#formulario")
 
 formulario.addEventListener('submit', (e) =>{
     e.preventDefault();
 let datos = Object.fromEntries(new FormData(e.target))
-let pago = 5208.33
-let horas = datos.hora
-horas *= pago
-pagar.innerHTML = horas
+let acumulador = 0
+let numero = datos.numero
+for (let i = 1; i <= numero/2; i++) {
+    if (numero % i == 0) {
+        acumulador += i
+    }
+    if (acumulador == numero) {
+        perfecto.innerHTML = `El numero "${numero}" es un numero perfecto`
+    }
+    else{
+        perfecto.innerHTML = `El numero "${numero}" no es un numero perfecto`
+    }
+
+}
+
+
+
 })
 
 })
